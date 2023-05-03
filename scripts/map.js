c8b2cbeba39ed04d41d4ef8287753bd3e485facf
Ponneelan <ponneelan1305@gmail.com>
2023-05-03 15:31:09 +0530

    map view changed into mapbox api

--- scripts/map.js
+++ scripts/map.js
@@ -1,10 +1,16 @@
-var mapOptions = {
-    center: [ 13.060027592555944, 80.22105486620737],
-    zoom: 11
- }
- 
- var map = new L.map('map', mapOptions);
- 
- var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
- 
- map.addLayer(layer);
+mapboxgl.accessToken = 'pk.eyJ1IjoicG9ubmVlbGFuIiwiYSI6ImNsaDdoZTkzMzBnNm4zcW5za3FrZDM2eTQifQ.jXGUIbCywbLynh8A6GOZCw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [80.22105486620737,13.060027592555944 ],
+    zoom: 13,
+    navigationControl: {
+        showCompass: true,
+        showZoom: true
+    }
+});
+
+// Add a pinpoint at your location
+var marker = new mapboxgl.Marker({color: 'red'})
+    .setLngLat([80.22105486620737,13.060027592555944 ])
+    .addTo(map);
